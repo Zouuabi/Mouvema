1c1c9a89f92c89f7f4b598645ce20f66e5b8942a
Oubeid Zouabi <zouabifoued8@gmail.com>
2025-09-30 20:27:25 +0100

    before gemini

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mouvema/firebase_options.dart';
 
 import 'src/app.dart';
 
-import 'src/data/data_source/remote_data_source/firebase_options.dart';
 import 'src/injector.dart';
 
 void main() async {
